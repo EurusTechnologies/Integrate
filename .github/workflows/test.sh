f84801dbb6771ea161195df17174cb987dedadce
hasnaineurus <akblessing1985@gmail.com>
2023-05-16 21:10:04 +0500

    changes v2

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -8,7 +8,7 @@ exec 3<&-
 for item in "${REPOS[@]}"; do
   ENVIRONMENT_NAMES=$(curl -L \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer ${{env.EGITHUB_TOKEN}}" \
+  -H "Authorization: Bearer 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" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   "https://api.github.com/repos/EurusTechnologies/$item/environments")
   echo $ENVIRONMENT_NAMES
