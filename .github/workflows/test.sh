57bca92b76e3a02611476a7551001aced24439b6
hasnaineurus <akblessing1985@gmail.com>
2023-05-16 21:02:31 +0500

    v6

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -1,5 +1,15 @@
-Name=$(curl -L \
+REPOS=()
+exec 3< "includedRepos.txt"
+# Read the file line by line
+while IFS= read -r line <&3 || [ -n "$line" ]; do
+  REPOS+=("$line")
+done
+exec 3<&-
+for item in "${REPOS[@]}"; do
+  ENVIRONMENT_NAMES=$(curl -L \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $GITHUB_TOKEN" \
+  -H "Authorization: Bearer ${{env.EGITHUB_TOKEN}}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  "https://api.github.com/repos/EurusTechnologies/$repo/environments")
+  "https://api.github.com/repos/EurusTechnologies/$item/environments")
+  echo $ENVIRONMENT_NAMES
+done
